e43bcc2193956f1ea35226e27accfe8ba4300197
Daryck <daryck.brown@nymbuscapital.net>
2025-11-11 17:20:33 -0500

    bugfix - DB_url error - added missing load_doenv to config for Settings

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Settings(BaseModel):
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY", "")
     DATABASE_URL: str = os.getenv("DATABASE_URL", "")
